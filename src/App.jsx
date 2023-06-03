1f35d1fa71d561a57c406501d94477c291d2338e
sugiartofahmi <sugiartofahmi@gmail.com>
2023-06-04 02:10:11 +0700

    improvement: atomic design

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <>
-      <h1 className="text-5xl font-bold">Hello </h1>
-    </>
-  );
-};
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <>
+      <h1 className="text-5xl font-bold">Hello </h1>
+    </>
+  </StrictMode>
+);
